20884233c9486450de4f157cba45adfdb70cce2e
Glitch (noodles-file-metadata) <none>
2017-11-01 04:01:53 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const multer = require('multer');
+const upload = multer()
 const port = process.env.PORT;
 
 const app = express();
 
-app.use(express.static('pulbic/'));
+app.use(express.static('public/'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 })
 
-app.get('/metadata', (req, res) => {
-  console.log('Metadata');
+app.get('/metadata', upload.array(), (req, res) => {
+  console.log(req);
 });
 
 app.listen(port);
