d1435b047cbf72d3e6332d257a25c01dc0a35c42
Glitch (noodles-file-metadata) <none>
2017-11-02 04:18:31 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer()
-const port = process.env.PORT;
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage
+})
 
+const port = process.env.PORT;
 const app = express();
 
 app.use(express.static('public/'));
@@ -11,19 +14,21 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 })
 
-app.get('/metadata', upload.single('target'), (req, res) => {
+app.post('/metadata/', upload.array([{name:'target', maxCount:1}]), (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'application/javascript'
   });
   
-  console.log(req.target);
+  console.log(req.files);
   
   let json = {
     'name': '',
     'size': ''
   };
   
-  json.size = req.file;
+  
+  // json.name = req.file.originalname;
+  // json.size = req.file.size;
   
   res.end(JSON.stringify(json, null, 3));
 });
